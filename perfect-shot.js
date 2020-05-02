8ca9a0b3c4490fecec063ee63e87aa79eb26cc68
Meghan Hein <meghan.hein@gmail.com>
2020-05-02 22:10:44 +0000

    working with looping over arrays

---
+++ perfect-shot.js
@@ -0,0 +1,21 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north'] 
+
+const finalPosition = function (moves) {
+  let position = [0, 0];
+  
+  for (let dir of moves) {
+    
+    switch(dir) {
+    case 'north' : position[1] += 1;
+      break;
+    case 'south' : position[1] -= 1;
+      break;
+    case 'west' : position[0] -= 1;
+      break;
+    case 'east' : position[0] += 1;
+      break;
+
+    }
+  }
+  return position
+}
