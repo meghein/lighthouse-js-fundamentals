9b6e89c9806a917e3235a357cb117a1dc5d6a306
Meghan Hein <meghan.hein@gmail.com>
2020-05-03 16:16:08 +0000

    challenge using functions and arithematic operators

---
+++ hundreds.js
@@ -0,0 +1,12 @@
+const howManyHundreds = function(num) {
+  var x = num;
+	var y = num % 100;
+	var z = x - y;
+	return z / 100;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
